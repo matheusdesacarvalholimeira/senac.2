92f02831c10ecdde930601016d66f86939aa9234
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2023-05-12 18:59:13 -0300

    Add files via upload

---
+++ SistemaSenac-main_v2-main/login/scripts/script_log.js
@@ -0,0 +1,13 @@
+const botlog = document.querySelector(".btn")
+const sou = document.querySelector(".sou").value
+botlog.addEventListener("click", ()=>{
+    console.log("funfo")
+    if(sou == "aluno"){
+
+    }else if(sou == "professor"){
+
+    }else if(sou == "professor"){
+        
+    }
+    
+})
